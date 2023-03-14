4b07105375152ddcccf87ebcc557b96f65d5ccb8
Hao <huanghao.mail@qq.com>
2023-03-14 14:07:14 +0800

    Asset
    new asset icon added.

--- pages/material/material.js
+++ pages/material/material.js
@@ -1,6 +1,15 @@
 Page({
   data: {
-      
+      icon: {
+        "right": ["/asset/circle-green.png"],
+        "right_filled": ['/asset/circle-green-filled.png'],
+        "wrong": ["/asset/circle-red-filled.png"],
+        "blank": ["/asset/circle-gray.png"]
+      },
+    
+      correct_answer: [1,1,0,0],
+      user_answer: [1,0,1,0],
+      options: ['A','B','C','D']
   },
   onShow(e) {
   }
